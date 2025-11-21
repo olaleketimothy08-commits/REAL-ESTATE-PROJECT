17ff378f12795310d12d5ab72f18f361f689e9a3
olaleketimothy08 <olaleketimothy08@gmail.com>
2025-11-21 09:50:36 +0000

    changed

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
   date.innerHTML = new Date().toDateString ()
 
   date.style.color ='grey'
+
+  function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
